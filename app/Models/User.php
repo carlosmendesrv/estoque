a753afe3d2ef42e8f998d8a31b05db421ca97060
Julio Silva <julio20.cesar02@gmail.com>
2021-10-11 10:51:15 -0300

    Add permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Traits\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles, Uuid;
+
+    public $incrementing = false;
 
     /**
      * The attributes that are mass assignable.
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Permission;
+use App\Models\Role;
 use Illuminate\Support\ServiceProvider;
+use Ramsey\Uuid\Uuid;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /* Begin : UUID Adjustment */
+        Permission::retrieved(function (Permission $permission) {
+            $permission->incrementing = false;
+        });
+
+        Permission::creating(function (Permission $permission) {
+            $permission->incrementing = false;
+            $permission->id = Uuid::uuid4()->toString();
+        });
+
+        Role::retrieved(function (Role $role) {
+            $role->incrementing = false;
+        });
+
+        Role::creating(function (Role $role) {
+            $role->incrementing = false;
+            $role->id = Uuid::uuid4()->toString();
+        });
+        /* End : UUID Adjustment */
     }
 }
